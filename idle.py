8b99c1e97d22946636a6fb565163ca0182f21c4e
Ray Schulz <rascul3@gmail.com>
2014-02-13 14:49:13 -0500

    fix idler thing

--- idle.py
+++ idle.py
@@ -1,31 +1,34 @@
+"""
+Keeps you from idling out on wotmud. Automagically resets idle counter
+ever time there's input from the user.
+"""
+__author__ = "Ray Schulz"
+__version__ = "1.0"
+__date__ = "February 13, 2014"
+
 from lyntin import exported
 
-# this holds the lyntin tick value of the last time i executed a lyntin command
-lyntin_tick = 0
-
-# keep track if we're idling longer than 60 seconds
-idle_flag = 0
-
-# tie current tick
-current_tick = 0
+# keep track of how long we've been idle
+idle_ticks = 0
 
 def handle_timer(args):
-	global idle_flag, lyntin_tick, current_tick
+	global idle_ticks
 	
-	current_tick = args["tick"]
-	delta = current_tick - lyntin_tick
-	if delta > 60:
-		idle_flag = 1
-		#exported.lyntin_command("#showme idle for %d seconds" % delta, internal=1)
+	idle_ticks += 1
+	
+	if idle_ticks >= 200:
+		exported.write_message("You have been idle.", ses=exported.get_current_session())
+		
+		# send a carraige return, but with #raw instead of #cr so it doen't
+		# conflict with repeatenter
+		exported.lyntin_command("#raw", internal=1, session=exported.get_current_session())
+		
+		idle_ticks = 0
 
 def handle_from_user(args):
-	global idle_flag, lyntin_tick, current_tick
+	global idle_ticks
 	
-	if idle_flag == 1:
-		#exported.lyntin_command("#showme stopping idle timer", internal=1)
-		idle_flag = 0
-	
-	lyntin_tick = current_tick
+	idle_ticks = 0
 
 def load():
 	exported.hook_register("timer_hook", handle_timer)
