b08892f2f6ee0c039822358f8453c7699b275518
Ray Schulz <rascul3@gmail.com>
2014-02-14 00:48:55 -0500

    rewrite to handle multiple sessions individually, ignore common session and only reset timer when stuff gets sent to the mud

--- idle.py
+++ idle.py
@@ -3,37 +3,50 @@ Keeps you from idling out on wotmud. Automagically resets idle counter
 ever time there's input from the user.
 """
 __author__ = "Ray Schulz"
-__version__ = "1.0"
-__date__ = "February 13, 2014"
+__version__ = "2.0"
+__date__ = "February 14, 2014"
 
-from lyntin import exported
+from lyntin import exported, session
 
 # keep track of how long we've been idle
-idle_ticks = 0
+ticks = dict()
+
+def handle_connect(args):
+	# add new session to ticks dict except for the common session
+	global ticks
+	if args['session'].getName() is not 'common':
+		ticks[args['session']] = 0
+
+def handle_disconnect(args):
+	# remove disconnected session from ticks dict
+	global ticks
+	del ticks[args['session']]
 
 def handle_timer(args):
-	global idle_ticks
+	# tick tock
+	global ticks
 	
-	idle_ticks += 1
-	
-	if idle_ticks >= 200 and str(exported.get_current_session()) != "session.Session common":
-		exported.write_message("You have been idle.", ses=exported.get_current_session())
+	for session in ticks.keys():
+		ticks[session] += 1
 		
-		# send a carraige return, but with #raw instead of #cr so it doen't
-		# conflict with repeatenter
-		exported.lyntin_command("#raw", internal=1, session=exported.get_current_session())
-		
-		idle_ticks = 0
+		if ticks[session] >= 200:
+			exported.write_message("You have been idle.", ses=session)
+			exported.lyntin_command("#raw", internal=1, session=session)
+			ticks[session] = 0
 
-def handle_from_user(args):
-	global idle_ticks
-	
-	idle_ticks = 0
+def handle_to_mud(args):
+	global ticks
+	if args['session'].getName() is not 'common':
+		ticks[args['session']] = 0
 
 def load():
+	exported.hook_register("connect_hook", handle_connect)
+	exported.hook_register("disconnect_hook", handle_connect)
 	exported.hook_register("timer_hook", handle_timer)
-	exported.hook_register("from_user_hook", handle_from_user)
+	exported.hook_register("to_mud_hook", handle_to_mud)
 
 def unload():
+	exported.hook_unregister("connect_hook", handle_connect)
+	exported.hook_unregister("disconnect_hook", handle_connect)
 	exported.hook_unregister("timer_hook", handle_timer)
-	exported.hook_unregister("from_user_hook", handle_from_user)
+	exported.hook_unregister("to_mud_hook", handle_to_mud)
